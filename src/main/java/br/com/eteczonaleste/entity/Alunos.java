553134ed428f36b20ec7f9f95610e369afb767fe
wagnermarques <wagnerdocri@gmail.com>
2016-11-04 13:04:16 -0200

    primeiro commit (importacao do projeto)

---
+++ src/main/java/br/com/eteczonaleste/entity/Alunos.java
@@ -0,0 +1,121 @@
+package br.com.eteczonaleste.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "alunos")
+public class Alunos {
+
+     @Id
+    private Integer ra;
+    @Column
+    private String nome;
+    @Column
+    private String endereco;
+    @Column
+    private String bairro;
+    @Column
+    private String cep;
+    @Column
+    private String cidade;
+    @Column
+    private String estado;
+    @Column
+    private String telefone;
+    @Column
+    private String celular;
+    @Column
+    private String email;
+    @Column
+    private String template;
+
+   
+
+    public Integer getRa() {
+        return ra;
+    }
+
+    public void setRa(Integer ra) {
+        this.ra = ra;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public String getTemplate() {
+        return template;
+    }
+
+    public void setTemplate(String template) {
+        this.template = template;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ src/main/java/br/com/eteczonaleste/entityManager/AlunosManager.java
@@ -0,0 +1,61 @@
+package br.com.eteczonaleste.entityManager;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import br.com.eteczonaleste.entity.Alunos;
+import java.util.Collection;
+import javax.persistence.Query;
+
+public class AlunosManager {
+
+    EntityManagerFactory emf;
+    EntityManager em;
+
+    public AlunosManager() {
+        emf = Persistence.createEntityManagerFactory("e211PU");
+        em = emf.createEntityManager();
+    }
+
+    public void insert(Alunos alunos) {
+        em.getTransaction().begin();
+        em.merge(alunos);
+        em.getTransaction().commit();
+        emf.close();
+    }
+
+    public Alunos findSelectDigital(String digital) {
+        Alunos alunoPesq = (Alunos) em.createQuery("SELECT a FROM Alunos a WHERE a.template=:maoesquerdapolegar")
+                .setParameter("maoesquerdapolegar", digital)
+                .getSingleResult();
+        return alunoPesq;
+    }
+    
+    public Alunos findByRm(int rm) {
+        Alunos alunosPesq = (Alunos) em.createQuery("SELECT a FROM Alunos a WHERE a.ra=:regm")
+                .setParameter("regm", rm)
+                .getSingleResult();
+        return alunosPesq;
+    }
+    
+    public Alunos findByName(String nome) {
+        Alunos alunosPesq = (Alunos) em.createQuery("SELECT a FROM Alunos a WHERE a.nome=:nome")
+                .setParameter("nome", nome)
+                .getSingleResult();
+        return alunosPesq;
+    }
+    
+    public Collection<Alunos> findSelectAll() {
+        Query alunoPesq = em.createQuery("SELECT s FROM Alunos s");
+        
+        return (Collection<Alunos>) alunoPesq.getResultList();
+    }
+
+    public void delete(AlunosManager alunos) {
+        em.getTransaction().begin();
+        em.remove(alunos);
+        em.getTransaction().commit();
+        emf.close();
+    }
+
+}
